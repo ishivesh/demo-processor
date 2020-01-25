82e11f09d624c9feb342837810a29972d7d8a566
Shivesh Kumar <shivesh.kumar@prime-research.com>
2020-01-25 13:35:33 +0100

    Initial commit

---
+++ src/main/java/com/shiv/scdf/example/demoprocessor/DemoProcessorApplication.java
@@ -0,0 +1,35 @@
+package com.shiv.scdf.example.demoprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.Executor;
+import java.util.function.Function;
+
+@SpringBootApplication
+public class DemoProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProcessorApplication.class, args);
+    }
+
+    @Bean
+    public Function<Flux<String>, Flux<String>> process() {
+        return stringFlux -> stringFlux
+            .map(
+                s -> {
+                    try {
+                        Thread.sleep(5000L);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return s;
+                })
+            .map(s -> s.toUpperCase())
+            .log();
+    }
+}
